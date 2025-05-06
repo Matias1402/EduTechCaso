24e4dcab9ac5863f7d123af255181b79e93f4bb3
Matias1402 <matias18torres18@gmail.com>
2025-05-06 14:51:20 -0400

    Prueba 2

--- src/main/java/com/edutech/cl/EduTech/EduTechApplication.java
+++ src/main/java/com/edutech/cl/EduTech/EduTechApplication.java
@@ -3,6 +3,7 @@ package com.edutech.cl.EduTech;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //holaaaaaaaafsrfsst
+//aaaaa
 @SpringBootApplication
 public class EduTechApplication {
 
