82a0d93b7fb0644a484f07fb496c98a9d6352f84
Matias1402 <matias18torres18@gmail.com>
2025-05-13 16:47:53 -0400

    Avance en modelo del curso

---
+++ src/main/java/com/edutech/cl/EduTech/model/Curso.java
@@ -0,0 +1,26 @@
+package com.edutech.cl.EduTech.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Cursos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Curso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_curso;
+
+    @Column(unique = true, nullable = false)
+    private String Isbn;
+
+    @Column(nullable = false)
+    private String Nombre_Curso;
+
+
+}
